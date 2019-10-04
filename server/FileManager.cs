14d7c5556735621df3ef123874aa51e689dd6ade
Unknown <amchelle@avans.nl>
2019-10-04 14:33:30 +0200

    [Added] FIleManager class and SaveAppointment method
    FIleManager manages the files for the appointments and available dates.
    SaveAppointment saves the given appointment to a file with lists of Appointments as JSON strings.

---
+++ server/FileManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SharedProject;
+
+namespace server
+{
+    class FileManager
+    {
+        private DayData PossibleTimes;
+        private List<Appointment> appointments;
+
+        public FileManager()
+        {
+            appointments = new List<Appointment>();
+        }
+    }
+}
