4bbfc2d4bfcf44f5a7165788b9eb3430be4ab4fd
Unknown <amchelle@avans.nl>
2019-10-04 13:48:27 +0200

    [Added] Appointment class
    appointment stores the info of an appointment (username, day, time). can be saved and loaded as a JSON String.

---
+++ SharedProject/Appointment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace SharedProject
+{
+    class Appointment
+    {
+        [JsonProperty(PropertyName = "username")]
+        public string username { get; set; }
+
+        [JsonProperty(PropertyName = "day")]
+        public string day { get; set; }
+
+        [JsonProperty(PropertyName = "time")]
+        public string time { get; set; }
+
+        public Appointment()
+        { }
+
+        public string ToJSONString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
