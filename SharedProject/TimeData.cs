7fe06f75beab75983a3b48ca0f7422ba96105b5c
Unknown <amchelle@avans.nl>
2019-10-04 13:36:18 +0200

    [Added] TimeData class
    TimeData stores info about the possible appointment times a day can have. this data can be saved as an json string

---
+++ SharedProject/TimeData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedProject
+{
+    class TimeData
+    {
+        public string day { get; set; }
+        public List<string> times { get; set; }
+
+        public TimeData()
+        {
+            times = new List<string>();
+        }
+
+        public string ToJSONString()
+        {
+        }
+    }
+}
