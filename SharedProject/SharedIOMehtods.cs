0e01f735bc93689bc9e85c52a347a3b748b9b775
Unknown <amchelle@avans.nl>
2019-10-04 13:56:17 +0200

    [Added] SharedIOMethods class
    SharedIOMethods allows the user to send and recieve client/server messages

---
+++ SharedProject/SharedIOMehtods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Net.Sockets;
+
+namespace SharedProject
+{
+    class SharedIOMehtods
+    {
+        public static void WriteTextMessage(TcpClient client, string message)
+        {
+            var stream = new StreamWriter(client.GetStream(), Encoding.ASCII);
+            stream.WriteLine(message);
+            stream.Flush();
+        }
+
+        public static string ReadTextMessage(TcpClient client)
+        {
+            StreamReader stream = new StreamReader(client.GetStream(), Encoding.ASCII);
+            string line = stream.ReadLine();
+
+            return line;
+        }
+    }
+}
