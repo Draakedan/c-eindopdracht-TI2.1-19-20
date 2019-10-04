ffac9f6975dfb96c8c661d02272cbfc0673ddb89
Unknown <amchelle@avans.nl>
2019-10-04 14:50:36 +0200

    [Added] UpdateAvailableDates Method to FileManager
    UpdateAvailableDates removes the time string of the time and day of the appointment. the updated dayData object is saved as an json-txt file.

--- server/FileManager.cs
+++ server/FileManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using SharedProject;
+using System.IO;
 
 namespace server
 {
@@ -13,6 +14,48 @@ namespace server
         public FileManager()
         {
             appointments = new List<Appointment>();
+        }
+
+        public void SaveAppointment(Appointment appointment)
+        {
+            string filename = System.Environment.CurrentDirectory + "/appointments.txt";
+            appointments.Add(appointment);
+
+            var stream = new FileStream(filename, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            var writer = new StreamWriter(stream);
+            writer.WriteLine(appointment.ToJSONString());
+            writer.Flush();
+            writer.Close();
+            stream.Close();
+        }
+
+        public void UpdateAvailableDates(Appointment appointment)
+        {
+            TimeData data = new TimeData();
+            foreach (TimeData day in PossibleTimes.times)
+            {
+                if (day.day == appointment.day)
+                {
+                    data = day;
+                    break;
+                }
+            }
+
+            if (data.times.Contains(appointment.time))
+            {
+                data.times.Remove(appointment.time);
+                if (data.times.Count == 0)
+                    PossibleTimes.times.Remove(data);
+            }
+
+            string filename = System.Environment.CurrentDirectory + "/availableTimes.txt";
+
+            var stream = new FileStream(filename, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            var writer = new StreamWriter(stream);
+            writer.Write(PossibleTimes.ToJSONString());
+            writer.Flush();
+            writer.Close();
+            stream.Close();
         }
     }
 }
